f4c91856cc660aef8f125345ae21e9a46503253b
Uderr <uderr@LAPTOP-F3JHH70T.localdomain>
2021-02-14 19:25:53 +0100

    3 commit: edited CF

--- src/main/scala/CF.scala
+++ src/main/scala/CF.scala
@@ -1,10 +1,23 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
 import org.apache.spark.sql.SQLContext
-
-import org.apache.spark.mllib.recommendation.ALS
+import org.apache.spark.rdd.RDD
 
 object CF {
+
+  //SET OF SPARK ENVIRONMENT
+  val conf = new SparkConf().setAppName("CollFilt").setMaster("local[*]")
+  val sc = new SparkContext(conf)
+  val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+  //DECLARATION OF DATASETS
+  val data = sc.textFile("ratings.csv")
+  val movies = sc.textFile("movie.csv")
+
+
+  println("ArrivePoint1")
+
+  println("ArrivePoint2")
 
   //FUNCTION USED TO CREATE RATING OBJECT
   def ratingCreation(line: String): Rating = {
@@ -12,64 +25,47 @@ object CF {
     Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble)
   }
 
-  def topRated() = {
+  //ALGORITHM ALS
+  def ALSAlgo(trainingset: RDD[Rating]): MatrixFactorizationModel = {
+    val rank = 6; val iter = 5; val lambda = 0.01
+    val algoALS = ALS.train(trainingset,rank,iter,lambda)
+    algoALS
+  }
+
+  //
+  def topRatedForKnownUser(user: Int, numberOfPrediction: Int,algoALS: MatrixFactorizationModel): Double = {
+    val predictRating = algoALS.predict(user,numberOfPrediction)
+    predictRating
+  }
+
+  def topRatedForUnknownUser(): Unit = {
 
   }
 
-
-  def main(args: Array[String]): Unit = {
-
-    //SET OF SPARK ENVIRONMENT
-    val conf = new SparkConf().setAppName("CollFilt").setMaster("local[*]")
-    val sc = new SparkContext(conf)
-
-    //DECLARATION OF DATASETS
-    val data = sc.textFile("ratings.csv")
-    data.mapPartitionsWithIndex {
-      (idx, iter) => if (idx == 0) iter.drop(1) else iter
-    }
-    val movies = sc.textFile("movie.csv")
-
-    //CREATION OF TUPLES
-    val results = data.map(ratingCreation)
-    val titles = movies.map(line => line.split(",").take(2)).map(array => (array(0).toInt,array(1))).collectAsMap()
-
-    println("ArrivePoint1")
-
-    //SPLIT OF RATINGS DATASET
-    val Array(trainingset,dataset) = results.randomSplit(Array(0.9,0.1))
-    println("ArrivePoint2")
-
-    //SET OF ALS ALGORITHM
-    val rank = 6; val iter = 5; val lambda = 0.01
-    val algoALS = ALS.train(trainingset,rank,iter,lambda)
-
-
-    val usersProducts = results.map { case Rating(user, product, rate) => (user, product)}
-    //usersProducts.foreach(println)
-
+  //MEAN SQUARE ERROR OF THE MODEL
+  def MSE(algoALS: MatrixFactorizationModel, usersProducts: RDD[(Int,Int)], results: RDD[Rating]): Double = {
     val predictions = algoALS.predict(usersProducts).map { case Rating(user, product, rate) => ((user, product), rate)}
-    //predictions.foreach(println)
-
+    predictions.foreach(println)
     val ratesAndPreds = results.map { case Rating(user, product, rate) => ((user, product), rate)}.join(predictions)
-    //ratesAndPreds.foreach(println)
-
-    //MEAN SQUARED ERROR
     val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) => val err = (r1 - r2)
       err * err
     }.mean()
-    println(MSE)
+    MSE
+  }
 
-    //EXAMPLE
-    val predictRating = algoALS.predict(3,102)
-    println(predictRating)
+  def makeAPrediction(arg1: Int, arg2: Int,algoALS: MatrixFactorizationModel): Long = {
+    val predictRating = algoALS.predict(arg1,arg2)
+    val prediction = predictRating.round
+    prediction
+  }
 
-    val userId = 789
-    val K = 10
-    //val topKRecs = algoALS.recommendProducts(userId, K)
-    //println(topKRecs.mkString("\n"))
-
+  def main(args: Array[String]): Unit = {
+    //CREATION OF TUPLES
+    val results = data.map(ratingCreation)
+    val titles = movies.map(line => line.split(",").take(2)).map(array => (array(0).toInt,array(1))).collectAsMap()
+    //SPLIT OF RATINGS DATASET
+    val Array(trainingset,dataset) = results.randomSplit(Array(0.9,0.1))
+    trainingset
 
   }
 }
-
